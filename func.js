3b1a933d5fbb838c9d2038a3291e3eb0ad2763d5
AstroBesat <128177174+AstroBesat-SoftW@users.noreply.github.com>
2023-09-20 01:06:25 +0300

    Update func.js
    old

--- func.js
+++ func.js
@@ -2,40 +2,37 @@ document.addEventListener('DOMContentLoaded', function() {
     const sesAlgilamaButon = document.getElementById('sesAlgilamaButon');
     const algilananMetin = document.getElementById('algilananMetin');
     const baslaButon = document.getElementById('baslaButon');
-    const durButon = document.getElementById('durButon');
+    //const durButon = document.getElementById('durButon');
     let recognition; // Recognition nesnesini global olarak tanımlayın
 
     // Başlat Butonuna Tıklama Olayını Ekle
     baslaButon.addEventListener('click', function() {
         recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition || window.mozSpeechRecognition || window.msSpeechRecognition)();
         recognition.lang = 'tr-TR'; // Algılama Dilini Türkçe olarak ayarlayın
-/*
-        recognition.onresult = function(event) {
-            const sonuc = event.results[0][0].transcript;
-            algilananMetin.value = sonuc.replace(/\s+/g, ''); // Algılanan metni input kutusuna yaz
-        };
 
-normalde öyledi fakat çarpı dediğimde x algılıyor bu da hesaplama hatası oluyor bende x i * e çevirdim
-*/
 recognition.onresult = function(event) {
     const sonuc = event.results[0][0].transcript;
     const duzenlenmisSonuc = sonuc.replace(/\s+/g, '').replace(/x/g, '*'); // Algılanan metni düzenle ve "*" karakterini ekleyin
     algilananMetin.value = duzenlenmisSonuc; // Düzenlenmiş metni input kutusuna yaz
 };
 
-        recognition.onend = function() {
-            recognition.start(); // Algılamayı tekrar başlat
-        };
+        
+    
 
         recognition.start(); // Algılamayı Başlat
-        baslaButon.disabled = true; // Başlat Butonunu devre dışı bırak
-        durButon.disabled = false; // Durdur Butonunu etkinleştir
-    });
+        if (baslaButon.textContent === "Başlat") {
+            baslaButon.textContent = "Durdur";
+            // Durdur adını veriyorum
+        } else {
+            baslaButon.textContent = "Başlat";
+              recognition.stop(); // Algılamayı Durdur
+            // Başlat ismini veriyorum
+        }
 
-    // Durdur Butonuna Tıklama Olayını Ekle
-    durButon.addEventListener('click', function() {
-        recognition.stop(); // Algılamayı Durdur
-        baslaButon.disabled = false; // Başlat Butonunu etkinleştir
-        durButon.disabled = true; // Durdur Butonunu devre dışı bırak
+
+
     });
+   
+
+
 });
