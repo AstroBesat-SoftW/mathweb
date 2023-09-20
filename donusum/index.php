4975cf2d1b88b7dbb4c7317ac1425c7951f534f6
AstroBesat <128177174+AstroBesat-SoftW@users.noreply.github.com>
2023-09-21 02:50:17 +0300

    Update index.php
    old page

--- donusum/index.php
+++ donusum/index.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hesap Makinesi</title>
     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"> <!-- icon eklecem -->
+
 </head>
 <body>
     <div class="calculator">
